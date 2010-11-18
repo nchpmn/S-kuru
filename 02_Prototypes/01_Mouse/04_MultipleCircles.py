eb74505456b3a5c192f469a831558af2e2c21372
Nathan Chapman <job.nchapman@gmail.com>
2010-11-19 09:56:25 +1100

    Circles staying on screen after drawn, framerate issues

---
+++ 02_Prototypes/01_Mouse/04_MultipleCircles.py
@@ -0,0 +1,36 @@
+import pygame
+
+screen = pygame.display.set_mode((800,600))
+
+running = True
+mouseIsDown = False
+circleCentre = (0,0)
+r = 10
+circles = []
+
+
+while running:
+    screen.fill((92,92,92))
+
+    for item in circles:
+        pygame.draw.circle(screen, (236,236,236), item[0], item[1])
+        
+    if mouseIsDown == True:
+        print "MouseHeld!", pygame.mouse.get_pos(), circleCentre, r
+        pygame.draw.circle(screen, (236,236,236), circleCentre, r)
+        circles.append([circleCentre, r])
+        r += 1
+
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        running = False
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+        print "MouseDown", pygame.mouse.get_pos()
+        circleCentre = pygame.mouse.get_pos()
+        mouseIsDown = True
+    elif event.type == pygame.MOUSEBUTTONUP:
+        print "MouseUp", pygame.mouse.get_pos()
+        mouseIsDown = False
+        r = 10
+
+    pygame.display.flip() # Shows drawn frame from buffer
