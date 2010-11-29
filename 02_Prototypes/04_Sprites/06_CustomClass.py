e203725dfd75005d759b10547fed7c0985558ece
Nathan Chapman <job.nchapman@gmail.com>
2010-11-30 10:34:03 +1100

    Multiple balls, diagional directions, FPS limit

--- 02_Prototypes/04_Sprites/06_CustomClass.py
+++ 02_Prototypes/04_Sprites/06_CustomClass.py
@@ -1,10 +1,48 @@
 import pygame
 
-class Ball():
-    def __init__(self, pos, speed):
+# Classes -------------------------------------------------
+
+class Ball(): # No PyGame sprite this time... let's see if it works!
+    def __init__(self, pos, Xspeed, Yspeed):
         self.position = pos
-        self.movement = speed
+        self.x = Xspeed
+        self.y = Yspeed
 
         self.image = pygame.image.load("Ball1.png")
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.position
 
-    def update(self, 
+    def update(self, rect, x, y):
+        rect[0] += x
+        rect[1] += y
+        print self.position, rect
+
+
+# Main ----------------------------------------------------
+
+screen = pygame.display.set_mode((600,400))
+balls = []
+running = True
+
+FPSclock = pygame.time.Clock() # Limit the FPS
+
+for initialPos, xMove, yMove in [([20,30], 1, 0),
+                                 [[45,126], 0, 1],
+                                 ([200,85], 2, 1),
+                                 ([500,4], 5, 8)]:
+    balls.append(Ball(initialPos, xMove, yMove))
+
+while running:
+    FPSclock.tick(60) # Set the framerate here
+    screen.fill((92,92,92))
+
+    for b in balls:
+        b.update(b.rect, b.x, b.y)
+        screen.blit(b.image, b.rect)
+
+    event = pygame.event.poll()
+
+    if event.type == pygame.QUIT:
+        running = False
+
+    pygame.display.flip()
